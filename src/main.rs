9d50c0ea0e0fa2aa4f75be55353c0b71b4b1f59d
arjav0703 <arjavjain0703@gmail.com>
2025-05-13 18:00:36 +0530

    advanced tuples

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 fn main() {
     println!("Hellow World!");
 
-    let x: i32 = -50000000; // 32 bit one can't store much value
-    let y: i64 = -50000000000000;
+    let _x: i32 = -50000000; // 32 bit one can't store much value
+    let _y: i64 = -50000000000000;
 
-    // tuples
-    let cord: (char, i32) = ('a', 4);
+    // tuples: fix in length
+    let cords: (char, i32) = ('a', 4);
+    let _foo = cords.1 
+    // Destructuring
+    let (_c1, _c2) = cords;
 }
