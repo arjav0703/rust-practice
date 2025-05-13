ea665c4ab2e443ce9c0b442db82e8c4fd27fd73f
arjav0703 <arjavjain0703@gmail.com>
2025-05-13 18:12:47 +0530

    control flow

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,20 @@ fn main() {
     let _x: i32 = -50000000; // 32 bit one can't store much value
     let _y: i64 = -50000000000000;
 
-    // tuples: fix in length
+    // tuples: fix in length =================
     let cords: (char, i32) = ('a', 4);
-    let _foo = cords.1 
+    let _foo = cords.1;
     // Destructuring
     let (_c1, _c2) = cords;
+
+    //===== ARRAYS ========
+    let _arr = ["3", "1", "6", "223", "43", "2"];
+    let _item1 = _arr[0];
+
+    let temp: u32 = 30;
+    if temp > 20 {
+        println!("It's hot outside")
+    } else {
+        println!("It's cold outside")
+    }
 }
