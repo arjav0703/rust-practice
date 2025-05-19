350534c9b32d7fed87d9ea87c3c128eea30add10
arjav0703 <arjavjain0703@gmail.com>
2025-05-19 21:40:04 +0530

    better formatting

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -11,5 +11,5 @@ fn main() {
     println!("[info] Looking for '{}' in '{}'", &to_find, &file_path);
     
     let content = fs::read_to_string(file_path).expect("File not found");
-    println!("{:?}", content)
+    println!("{}", content)
 }
