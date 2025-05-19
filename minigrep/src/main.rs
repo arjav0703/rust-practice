9bc1446def5de39f62146c475992bb60c43ce38c
arjav0703 <arjavjain0703@gmail.com>
2025-05-19 21:38:54 +0530

    read file

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -7,5 +8,8 @@ fn main() {
     let to_find = &args[1];
     let file_path = &args[2];
 
-    println!("info Looking for '{}' in '{}'", &to_find, &file_path);
+    println!("[info] Looking for '{}' in '{}'", &to_find, &file_path);
+    
+    let content = fs::read_to_string(file_path).expect("File not found");
+    println!("{:?}", content)
 }
