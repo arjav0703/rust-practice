54acfb78f819f5dc18a4e55360cf7b7b4fd08a13
arjav0703 <arjavjain0703@gmail.com>
2025-05-19 21:46:50 +0530

    refactor code

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -7,9 +7,13 @@ fn main() {
 
     let to_find = &args[1];
     let file_path = &args[2];
+    let content = read_file(to_find, file_path);
+}
 
-    println!("[info] Looking for '{}' in '{}'", &to_find, &file_path);
+fn read_file(to_find: &str, file_path: &str) -> String{
+
+    println!("[fn: read_file()] Looking for '{}' in '{}'", &to_find, &file_path);
     
-    let content = fs::read_to_string(file_path).expect("File not found");
-    println!("{}", content)
+    let content = fs::read_to_string(file_path).expect("Err reading file");
+    return content;
 }
