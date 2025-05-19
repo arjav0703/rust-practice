10d86d349c75078128a4f72367838fcf03f16521
arjav0703 <arjavjain0703@gmail.com>
2025-05-19 22:24:25 +0530

    refactor code

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,13 +1,16 @@
 use std::env;
 use std::fs;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    // dbg!(&args);
+struct Config {
+    to_find: String,
+    file_path: String,
+}
 
-    let to_find = &args[1];
-    let file_path = &args[2];
-    let content = read_file(to_find, file_path);
+fn parse_config(args: &[String]) -> Config {
+    let to_find = args[1].clone();
+    let file_path = args[2].clone();
+
+    Config{to_find, file_path}
 }
 
 fn read_file(to_find: &str, file_path: &str) -> String{
@@ -17,3 +20,13 @@ fn read_file(to_find: &str, file_path: &str) -> String{
     let content = fs::read_to_string(file_path).expect("Err reading file");
     return content;
 }
+
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    // dbg!(&args);
+    let cfg = parse_config(&args);
+    let content = read_file( &cfg.to_find, &cfg.file_path);
+    println!("{}", content);
+}
+
